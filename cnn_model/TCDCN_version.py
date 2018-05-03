c08f17c23db8378c0343730ed59c47e16c79d9d3
annacanal <31701548+annacanal@users.noreply.github.com>
2018-05-03 14:13:29 +0200

    Add files via upload

--- cnn_model/TCDCN_version.py
+++ cnn_model/TCDCN_version.py
@@ -1,180 +1,168 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-# Imports
-import numpy as np
 import tensorflow as tf
+import time
+from datetime import timedelta
+import math
+import random
+import numpy as np
+import load_dataset
 
-tf.logging.set_verbosity(tf.logging.INFO)
+#Adding Seed so that random initialization is consistent
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+
+##################################   INIT     ################################
+batch_size = 100
+# 20% of the data will automatically be used for validation
+validation_size = 0.2
+img_size = 150
+num_channels = 3 #RGB (if grey scale, num_channels=1)
+#num_classes = 2 #len(target_names) - 1
+classes = ['not_smiling','smiling'] # 0 = no smiling, 1=smiling
+# classes = ['female','male'] # 0 = no smiling, 1=smiling
+num_classes = len(classes)
+train_path='../dataset/dataset_smile/'
+model_dir= '/smile-modeltcdcn/smile.ckpt'
+# train_path='../dataset/dataset_gender/'
+# model_dir= '/gender-modeltcdcn/gender.ckpt'
+#load all the training and validation images and labels into memory
+data = load_dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
+print("Complete reading input data.")
+print("Number of files in Training-set:\t\t{}".format(len(data.train.labels)))
+print("Number of files in Validation-set:\t{}".format(len(data.valid.labels)))
+session = tf.Session()
+# Define the graph
+x = tf.placeholder(tf.float32, shape=[None, img_size,img_size,num_channels], name='x')
+## labels
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+W = tf.Variable(tf.zeros([150 * 150, num_classes]))
+b = tf.Variable(tf.zeros([num_classes]))
+
+# Network parameters
+filter_size_conv1 = 5
+num_filters_conv1 = 20
+filter_size_conv2 = 5
+num_filters_conv2 = 48
+filter_size_conv3 = 3
+num_filters_conv3 = 64
+filter_size_conv4 = 3
+num_filters_conv4 = 80
+fc_layer_size = 256
+
+
+################################ FUNCTIONS ######################3
+def create_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
 
 def create_biases(size):
     return tf.Variable(tf.constant(0.05, shape=[size]))
 
-def cnn_model_fn(features, labels, mode):
-  # Input Layer
-  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # AFWL, LFW and net images are cutted to 60x60 pixels, and have RGB color channel
-  input_layer = tf.reshape(features["x"], [-1, 60, 60, 3])
+def create_convolutional_layer(input,num_input_channels,conv_filter_size,num_filters):
+    weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
+    biases = create_biases(num_filters)
+    ## Create the convolutional layer
+    layer = tf.nn.conv2d(input=input,  filter=weights, strides=[1, 1, 1, 1],  padding="VALID")
+    layer += biases
+    ## Output of pooling is fed to Relu which is the activation function for us.
+    layer = tf.nn.relu(layer)
+    return layer
 
-  # Convolutional Layer #1
-  # Input Tensor Shape: [batch_size, 60, 60, 1]
-  # Output Tensor Shape: [batch_size, 56, 56, 20]
-  conv1 = tf.layers.conv2d(
-      inputs=input_layer,
-      filters=20,           # Computes 32 features using a 5x5 filter with ReLU activation.
-      kernel_size=[5, 5],
-      padding="valid",       # valid Padding
-      #strides=(1, 1), # by default strides=1
-      activation=tf.nn.relu,
-      use_bias = True,
-      bias_initializer=create_biases(20))
+def create_pool_layer(layer):
+    ##max-pooling.
+    layer = tf.nn.max_pool(value=layer, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1], padding="SAME")
+    return layer
 
-  # Pooling Layer #1
-  # First max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 56, 56, 20]
-  # Output Tensor Shape: [batch_size, 28, 28, 20]
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+def create_flatten_layer(layer):
+    layer_shape = layer.get_shape()
+    num_features = layer_shape[1:4].num_elements()
+    layer = tf.reshape(layer, [-1, num_features])
+    return layer
 
-  # Convolutional Layer #2
-  # Input Tensor Shape: [batch_size, 28, 28, 20]
-  # Output Tensor Shape: [batch_size, 24, 24, 48]
-  conv2 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=48,             # Computes 64 features using a 5x5 filter.
-      kernel_size=[5, 5],
-      padding="valid",
-      activation=tf.nn.relu,
-      use_bias=True,
-      bias_initializer=create_biases(20))
+def create_fc_layer(input, num_inputs, num_outputs, use_relu=True):
+    weights = create_weights(shape=[num_inputs, num_outputs])
+    biases = create_biases(num_outputs)
 
-  # Pooling Layer #2
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 24, 24, 48]
-  # Output Tensor Shape: [batch_size, 12, 12, 48]
-  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
-  # Convolutional Layer #3
-  # Input Tensor Shape: [batch_size, 12 ,12 , 48]
-  # Output Tensor Shape: [batch_size, 10, 10, 64]
-  conv3 = tf.layers.conv2d(
-      inputs=pool1,
-      filters=64,  # Computes 64 features using a 5x5 filter.
-      kernel_size=[3, 3],
-      padding="valid",
-      activation=tf.nn.relu,
-      use_bias=True,
-      bias_initializer=create_biases(20))
+    layer = tf.matmul(input, weights) + biases
+    if use_relu:
+        layer = tf.nn.relu(layer)
+    return layer
 
 
-  # Pooling Layer #3
-  # Second max pooling layer with a 2x2 filter and stride of 2
-  # Input Tensor Shape: [batch_size, 10, 10, 64]
-  # Output Tensor Shape: [batch_size, 5, 5, 64]
-  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
-
-  # Convolutional Layer #4
-  # Input Tensor Shape: [batch_size, 5 ,5 , 48]
-  # Output Tensor Shape: [batch_size, 3, 3, 64]
-  conv4 = tf.layers.conv2d(
-      inputs=pool3,
-      filters=80,  # Computes 64 features using a 5x5 filter.
-      kernel_size=[3, 3],
-      padding="valid",
-      activation=tf.nn.relu,
-      use_bias=True,
-      bias_initializer=create_biases(20))
+def show_progress(epoch, feed_dict_train, feed_dict_validate, val_loss):
+    acc = session.run(accuracy, feed_dict=feed_dict_train)
+    val_acc = session.run(accuracy, feed_dict=feed_dict_validate)
+    msg = "Training Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%},  Validation Loss: {3:.3f}"
+    print(msg.format(epoch + 1, acc, val_acc, val_loss))
 
 
-  # Flatten tensor into a batch of vectors
-  # Input Tensor Shape: [batch_size, 3, 3, 80]
-  # Output Tensor Shape: [batch_size, 3 * 3 * 80]
-  conv4_flat = tf.reshape(conv4, [-1, 3 * 3 * 80])
+def train(num_iteration):
+    global total_iterations
 
-  # Dense Layer:  Densely connected layer with 1024 neurons
-  # Input Tensor Shape: [batch_size, 3 * 3 * 80]
-  # Output Tensor Shape: [batch_size, 256]
-  dense = tf.layers.dense(inputs=conv4_flat, units=256, activation=tf.nn.relu)
+    for i in range(total_iterations,
+                   total_iterations + num_iteration):
 
-  # Add dropout operation; 0.6 probability that element will be kept
-  dropout = tf.layers.dropout(
-      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+        x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
+        x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(batch_size)
 
-  # Logits layer
-  # Input Tensor Shape: [batch_size, 256]
-  # Output Tensor Shape: [batch_size, 4]  as we have2 classes smile, no smile) or ( male, female)
-  logits = tf.layers.dense(inputs=dropout, units=2)
+        feed_dict_tr = {x: x_batch,
+                        y_true: y_true_batch}
+        feed_dict_val = {x: x_valid_batch,
+                         y_true: y_valid_batch}
 
-  predictions = {
-      # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1),
-      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-      # `logging_hook`.
-      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-  }
-  if mode == tf.estimator.ModeKeys.PREDICT:
-    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+        session.run(optimizer, feed_dict=feed_dict_tr)
 
-  # Calculate Loss (for both TRAIN and EVAL modes): cross entropy
-  onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
-  #loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-  loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+        if i % int(data.train.num_examples / batch_size) == 0:
+            val_loss = session.run(cost, feed_dict=feed_dict_val)
+            epoch = int(i / int(data.train.num_examples / batch_size))
+            show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+            saver.save(session, model_dir)
 
-  # Configure the Training Op (for TRAIN mode)
-  if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(
-        loss=loss,
-        global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-  # Add evaluation metrics (for EVAL mode)
-  eval_metric_ops = {
-      "accuracy": tf.metrics.accuracy(
-          labels=labels, predictions=predictions["classes"])}
-  return tf.estimator.EstimatorSpec(
-      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    total_iterations += num_iteration
 
 
-def main(unused_argv):
-  # Load training and eval data
-  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-  train_data = mnist.train.images  # Returns np.array
-  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-  eval_data = mnist.test.images  # Returns np.array
-  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+################################## NETWORK ####################################
 
-  # Create the Estimator
-  classifier = tf.estimator.Estimator(
-      model_fn=cnn_model_fn, model_dir="/tmp/model_tcdcn") #output dir
+layer_conv1 = create_convolutional_layer(input=x, num_input_channels=num_channels, conv_filter_size=filter_size_conv1,
+                                         num_filters=num_filters_conv1)
+layer_pool1 = create_pool_layer(layer_conv1)
 
-  # Set up logging for predictions
-  # Log the values in the "Softmax" tensor with label "probabilities"
-  tensors_to_log = {"probabilities": "softmax_tensor"}
-  logging_hook = tf.train.LoggingTensorHook(
-      tensors=tensors_to_log, every_n_iter=50)
+layer_conv2 = create_convolutional_layer(input=layer_pool1, num_input_channels=num_filters_conv1,
+                                         conv_filter_size=filter_size_conv2, num_filters=num_filters_conv2)
+layer_pool2 = create_pool_layer(layer_conv2)
 
-  # Train the model
-  train_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"x": train_data},
-      y=train_labels,
-      batch_size=100,
-      num_epochs=None,
-      shuffle=True)
-  classifier.train(
-      input_fn=train_input_fn,
-      steps=2000,
-      hooks=[logging_hook])
+layer_conv3 = create_convolutional_layer(input=layer_pool2, num_input_channels=num_filters_conv2,
+                                         conv_filter_size=filter_size_conv3, num_filters=num_filters_conv3)
+layer_pool3 = create_pool_layer(layer_conv3)
 
-  # Evaluate the model and print results
-  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-      x={"x": eval_data},
-      y=eval_labels,
-      num_epochs=1,
-      shuffle=False)
-  eval_results = classifier.evaluate(input_fn=eval_input_fn)
-  print(eval_results)
+layer_conv4 = create_convolutional_layer(input=layer_pool3, num_input_channels=num_filters_conv3,
+                                         conv_filter_size=filter_size_conv4, num_filters=num_filters_conv4)
+layer_pool4 = create_pool_layer(layer_conv4)
 
+layer_flat = create_flatten_layer(layer_pool4 )
 
-if __name__ == "__main__":
-  tf.app.run()
+layer_fc1 = create_fc_layer(input=layer_flat, num_inputs=layer_flat.get_shape()[1:4].num_elements(),
+                            num_outputs=fc_layer_size,  use_relu=True)
+
+layer_fc2 = create_fc_layer(input=layer_fc1, num_inputs=fc_layer_size, num_outputs=num_classes, use_relu=False)
+
+y_pred = tf.nn.softmax(layer_fc2, name='y_pred')
+
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+session.run(tf.global_variables_initializer())
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
+                                                        labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+session.run(tf.global_variables_initializer())
+total_iterations = 0
+saver = tf.train.Saver()
+
+#train(num_iteration=3000)
+train(num_iteration=3000)
+
