86a6fac5df03f446c400cc5655aba2fad7a70b19
annacanal <31701548+annacanal@users.noreply.github.com>
2018-05-07 15:18:13 +0200

    With inception layer

--- cnn_model/CascadeCNN.py
+++ cnn_model/CascadeCNN.py
@@ -15,15 +15,19 @@ set_random_seed(2)
 
 ##################################   INIT     ################################
 batch_size = 100
+learning_rate = 1e-3 #1e-4
 # 20% of the data will automatically be used for validation
 validation_size = 0.2
 img_size = 150
 num_channels = 3 #RGB (if grey scale, num_channels=1)
 #num_classes = 2 #len(target_names) - 1
 classes = ['not_smiling','smiling'] # 0 = no smiling, 1=smiling
+# classes = ['female','male'] # 0 = no smiling, 1=smiling
 num_classes = len(classes)
 train_path='../dataset/dataset_smile/'
 model_dir= '/smile-model/smile.ckpt'
+# train_path='../dataset/dataset_gender/'
+# model_dir= '/gender-model/gender.ckpt'
 #load all the training and validation images and labels into memory
 data = load_dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
 print("Complete reading input data.")
@@ -47,6 +51,7 @@ filter_size_conv3 = 3
 num_filters_conv3 = 60
 filter_size_conv4 = 2
 num_filters_conv4 = 80
+num_filters_conv5 = 100 #inception layer
 fc_layer_size = 160
 
 
@@ -62,15 +67,66 @@ def create_convolutional_layer(input,num_input_channels,conv_filter_size,num_fil
     biases = create_biases(num_filters)
     ## Create the convolutional layer
     layer = tf.nn.conv2d(input=input,  filter=weights, strides=[1, 1, 1, 1],  padding="VALID")
-    layer += biases
+    #layer += biases
+    layer = tf.nn.bias_add(layer, biases)
     ## Output of pooling is fed to Relu which is the activation function for us.
     layer = tf.nn.relu(layer)
     return layer
+
+
+def create_localconvolutional_layer(input,num_input_channels,conv_filter_size,num_filters):
+    weights = create_weights(shape=[conv_filter_size, conv_filter_size, num_input_channels, num_filters])
+    biases = create_biases(num_filters)
+    ## Create the convolutional layer
+    layer = tf.keras.layers.LocallyConnected2D(filters=num_filters, kernel_size =[conv_filter_size,conv_filter_size],
+                                               strides=[1, 1, 1, 1], padding = "valid", activation=tf.nn.relu,
+                                               use_bias= True, kernel_initializer= input, bias_initializer= biases)
+    #layer += biases
+    ## Output of pooling is fed to Relu which is the activation function for us.
+    #layer = tf.nn.relu(layer)
+    return layer
+
 
 def create_pool_layer(layer):
     ##max-pooling.
     layer = tf.nn.max_pool(value=layer, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
     return layer
+
+def create_inception2d(input, num_input_channels, num_filters):
+    # bias dimension = 3*filter_count and then the extra in_channels for the avg pooling
+    biases1 = create_biases(num_filters)
+    biasespre1 = create_biases(num_input_channels)
+    biasespre2 = create_biases(num_input_channels)
+    biases3 = create_biases(num_filters)
+    biases5 = create_biases(num_filters)
+    biasespost = create_biases(num_filters)
+
+    # 1x1
+    weights1 = create_weights(shape=[1, 1, num_input_channels, num_filters])
+    one_by_one_layer = tf.nn.conv2d(input=input, filter=weights1, strides=[1, 1, 1, 1], padding="SAME")
+    one_by_one_layer = tf.nn.bias_add(one_by_one_layer, biases1)
+    weights_pre1 = create_weights(shape=[1, 1, num_input_channels, num_input_channels])
+    pre_conv1= tf.nn.conv2d(input=input, filter=weights_pre1, strides=[1, 1, 1, 1], padding="SAME")
+    pre_conv1 =tf.nn.relu ( tf.nn.bias_add(pre_conv1, biasespre1))
+    weights_pre2 = create_weights(shape=[1, 1, num_input_channels, num_input_channels])
+    pre_conv2= tf.nn.conv2d(input=input, filter=weights_pre2, strides=[1, 1, 1, 1], padding="SAME")
+    pre_conv2 = tf.nn.relu((tf.nn.bias_add(pre_conv2, biasespre2)))
+    # 3x3
+    weights3 = create_weights(shape=[3, 3, num_input_channels, num_filters])
+    three_by_three_layer = tf.nn.conv2d(input=pre_conv1, filter= weights3, strides=[1, 1, 1, 1], padding="SAME")
+    three_by_three_layer = (tf.nn.bias_add(three_by_three_layer, biases3))
+    # 5x5
+    weights5 = create_weights(shape=[5, 5, num_input_channels, num_filters])
+    five_by_five_layer = tf.nn.conv2d(input=pre_conv2, filter= weights5, strides=[1, 1, 1, 1], padding="SAME")
+    five_by_five_layer = (tf.nn.bias_add(five_by_five_layer, biases5))
+
+    # avg pooling
+    pooling = tf.nn.max_pool(value= input, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], padding="SAME")
+    post_conv1 = tf.nn.conv2d(input=pooling, filter=weights1, strides=[1, 1, 1, 1], padding="SAME")
+    post_conv1 = (tf.nn.bias_add(post_conv1, biasespost))
+
+    inception1 = tf.nn.relu(tf.concat([one_by_one_layer, three_by_three_layer, five_by_five_layer, post_conv1], axis=3))
+    return inception1
 
 def create_flatten_layer(layer):
     layer_shape = layer.get_shape()
@@ -132,13 +188,20 @@ layer_pool2 = create_pool_layer(layer_conv2)
 
 layer_conv3 = create_convolutional_layer(input=layer_pool2, num_input_channels=num_filters_conv2,
                                          conv_filter_size=filter_size_conv3, num_filters=num_filters_conv3)
+# layer_conv3 = create_localconvolutional_layer(input=layer_pool2, num_input_channels=num_filters_conv2,
+#                                          conv_filter_size=filter_size_conv3, num_filters=num_filters_conv3)
+
 layer_pool3 = create_pool_layer(layer_conv3)
 
 layer_conv4 = create_convolutional_layer(input=layer_pool3, num_input_channels=num_filters_conv3,
                                          conv_filter_size=filter_size_conv4, num_filters=num_filters_conv4)
 layer_pool4 = create_pool_layer(layer_conv4)
 
-layer_flat = create_flatten_layer(layer_pool4 )
+#inception layer:
+layer_inception_5 = create_inception2d(input=layer_pool4, num_input_channels=num_filters_conv4, num_filters=num_filters_conv5)
+layer_pool5 = create_pool_layer(layer_inception_5)
+
+layer_flat = create_flatten_layer(layer_pool5)
 
 layer_fc1 = create_fc_layer(input=layer_flat, num_inputs=layer_flat.get_shape()[1:4].num_elements(),
                             num_outputs=fc_layer_size,  use_relu=True)
@@ -152,7 +215,7 @@ session.run(tf.global_variables_initializer())
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer_fc2,
                                                         labels=y_true)
 cost = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 correct_prediction = tf.equal(y_pred_cls, y_true_cls)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
